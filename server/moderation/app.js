29abfd3f96dddf0dafc76f82643b91389e2bac79
MID_Dev <MID_Dev>
2022-01-26 17:20:15 +0700

    moderation service initiallize

---
+++ server/moderation/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.post('/events', (req, res) => {
+
+})
+
+app.listen(4003, () => {
+    console.log('Moderation Service Listening on 4003')
+})
