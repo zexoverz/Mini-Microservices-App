a734e7481604e6d094f8d7298e4e666ab9ac871c
MID_Dev <MID_Dev>
2022-01-26 17:37:24 +0700

    Updating Comment Content

--- server/comments/app.js
+++ server/comments/app.js
@@ -47,6 +47,28 @@ app.post('/posts/:id/comments', async (req, res) => {
 app.post('/events', (req, res) => {
     console.log('Event Received', req.body.type)
 
+    const { type, data } = req.body
+    
+    if(type === 'CommentModerated'){
+        const { postId, id, status, content } = data
+
+        const comments = commentsByPostId[postId]
+
+        const comment = comments.find(comment => {
+            return comment.id === id
+        })
+
+        comment.status = status
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id, postId, content, status
+            }
+        });
+
+    }
+
     res.send({})
 })
 
