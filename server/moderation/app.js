ab6543058a83dbbe771221990fa8eacd02709988
MID_Dev <MID_Dev>
2022-01-26 17:31:23 +0700

    Handling moderation

--- server/moderation/app.js
+++ server/moderation/app.js
@@ -4,8 +4,24 @@ const axios = require('axios');
 
 const app = express();
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body
+    
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
 
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        })
+    }
+
+    res.send({})
 })
 
 app.listen(4003, () => {
