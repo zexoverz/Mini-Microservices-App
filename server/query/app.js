57ba0729b2ecaa5ed3df4bfaa8e6e940af140dc3
zexoverz <aryuical@gmail.com>
2022-02-03 17:23:48 +0700

    Edit Event-bus Address for other services

--- server/query/app.js
+++ server/query/app.js
@@ -54,7 +54,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
     console.log('Query Service Listening on 4002')
 
-    const res = await axios.get('http://localhost:4005/events').catch((err) => {
+    const res = await axios.get('http://event-bus-srv:4005/events').catch((err) => {
         console.log(err.message)
     })
 
